b799ee06ee83d201a351f688fe11aeea4ecd13e2
Brandon Sneyder Avilan Rodriguez <bsavilanr@unal.edu.co>
2018-03-05 21:01:22 +0000

    Final

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
     has_many :student_careers
-    has_many :students, through: :student_careers
+    has_many :students, through: :student_careerse
+    
+    validates :first_name, :last_name, presence: true, length:{ 
+        maximum: 15}
 end
